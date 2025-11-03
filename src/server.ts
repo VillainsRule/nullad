f245d692c0d1f54a3d645dc5399497c204fe3cdd
thonk <hello@villainsrule.xyz>
2025-11-03 11:39:41 -0500

    add ALT_PASSWORD to bypass cf check

--- src/server.ts
+++ src/server.ts
@@ -19,38 +19,41 @@ Bun.serve({
                 request.json().then(data => resolve(data)).catch(() => resolve({}));
             });
 
-            const json: { link: string; token: string } = jsonRaw as { link: string; token: string };
+            const json: { link: string, token: string, password?: string } = jsonRaw as any;
 
             if (!json || typeof json !== 'object') return sendInvalid('invalid json body');
             if (!('link' in json) || typeof json.link !== 'string') return sendInvalid('missing link');
             if (!DOMAINS.some((domain) => json.link.startsWith('https://' + domain))) return sendInvalid('that doesn\'t look like a linkvertise link...if this is a mistake, <a href="https://github.com/VillainsRule/nullad/issues" class="underline font-bold">open an issue</a>');
             if (!('token' in json) || typeof json.token !== 'string') return sendInvalid('missing captcha token');
 
-            const cfRequest = await fetch('https://challenges.cloudflare.com/turnstile/v0/siteverify', {
-                method: 'POST',
-                headers: { 'Content-Type': 'application/json' },
-                body: JSON.stringify({
-                    secret: process.env.CF_SECRETKEY,
-                    response: json.token,
-                    remoteip: request.headers.get('cf-connecting-ip') || undefined
-                })
-            });
+            if (('password' in json) && typeof json.password === 'string') {
+                if (json.password !== process.env.ALT_PASSWORD) return sendInvalid('invalid password');                
+            } else {
+                const cfRequest = await fetch('https://challenges.cloudflare.com/turnstile/v0/siteverify', {
+                    method: 'POST',
+                    headers: { 'Content-Type': 'application/json' },
+                    body: JSON.stringify({
+                        secret: process.env.CF_SECRETKEY,
+                        response: json.token,
+                        remoteip: request.headers.get('cf-connecting-ip') || undefined
+                    })
+                });
 
-            const cfResponse = await cfRequest.json();
+                const cfResponse = await cfRequest.json();
 
-            if (!cfResponse.success) {
-                const errorCode = cfResponse['error-codes'] ? cfResponse['error-codes'][0] : 'unknown error';
-                if (errorCode === 'missing-input-secret' || errorCode === 'invalid-input-secret')
-                    throw new Error('you have not properly setup cloudflare captchas, consult the README for more information');
+                if (!cfResponse.success) {
+                    const errorCode = cfResponse['error-codes'] ? cfResponse['error-codes'][0] : 'unknown error';
+                    if (errorCode === 'missing-input-secret' || errorCode === 'invalid-input-secret')
+                        throw new Error('you have not properly setup cloudflare captchas, consult the README for more information');
 
-                if (errorCode === 'missing-input-response' || errorCode === 'invalid-input-response')
-                    return sendInvalid('captcha failed, please reload & try again.');
+                    if (errorCode === 'missing-input-response' || errorCode === 'invalid-input-response')
+                        return sendInvalid('captcha failed, please reload & try again.');
 
-                return sendInvalid('an internal error occured, please reload the page & try again.');
+                    return sendInvalid('an internal error occured, please reload the page & try again.');
+                }
             }
 
             const result = await bypass(json.link);
-
             return new Response(JSON.stringify(result), { headers: { 'Content-Type': 'application/json' } });
         }
 
